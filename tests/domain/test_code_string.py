7828d21531dc174684e80efe4c9f71f0e71ff50f
Delavarga, David <David.Delavarga.consultant@nielseniq.com>
2022-04-07 11:11:57 +0200

    Test color not supported, support lowercase code

--- tests/domain/test_code_string.py
+++ tests/domain/test_code_string.py
@@ -1,6 +1,7 @@
 import pytest
 
 from mastermind.domain.models.code import CodeBuilder
+from mastermind.domain.models.exceptions import ColorNotSupportedError
 
 
 def test_from_code_string_to_data_structure():
@@ -10,3 +11,16 @@ def test_from_code_string_to_data_structure():
     assert code.pegs["G"] == {0}
     assert code.pegs["B"] == {1, 2}
     assert code.pegs["R"] == {3}
+
+
+def test_support_lowercase_colors():
+    code = "gbbr"
+    code_builder = CodeBuilder(code)
+    code = code_builder()
+    assert str(code) == "GBBR"
+
+
+def test_color_not_supported_in_builder():
+    code = "X"
+    with pytest.raises(ColorNotSupportedError):
+        CodeBuilder(code)
