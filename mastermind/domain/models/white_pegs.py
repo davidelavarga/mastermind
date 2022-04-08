fd227e6ad9105f43ca1f2c7ec9b4e2bd12fa71c2
Delavarga, David <David.Delavarga.consultant@nielseniq.com>
2022-04-08 08:00:32 +0200

    Improve white peg count

--- mastermind/domain/models/white_pegs.py
+++ mastermind/domain/models/white_pegs.py
@@ -10,11 +10,14 @@ class WhitePegs:
     code: Code
     guess: Code
 
+    def _white_peg_condition(self, color_data: Dict):
+        color, position = color_data
+        guess_pos = self.guess.pegs.get(color, set())
+        max_white_pegs = guess_pos - position
+        candidates = position - guess_pos
+        return min(len(max_white_pegs), len(candidates))
+
     def count(self) -> int:
-        white_pegs = 0
-        for color, position in self.code.pegs.items():
-            guess_pos = self.guess.pegs.get(color, set())
-            max_white_pegs = guess_pos - position
-            candidates = position - guess_pos
-            white_pegs += min(len(max_white_pegs), len(candidates))
-        return white_pegs
+        return reduce(
+            lambda x, y: x + y, map(self._white_peg_condition, self.code.pegs.items())
+        )
